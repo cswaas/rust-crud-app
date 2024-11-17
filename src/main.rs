1edd9c533426810324774cd0169284900c90b700
cswaas <46220990+cswaas@users.noreply.github.com>
2024-11-17 19:16:13 +0100

    Part 3 completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use crate::controller::{create_user, delete_user, get_user_by_id, list_users, update_user};
+use crate::user_service::UserService;
+use axum::{
+    routing::{delete, get, post, put},
+    Extension, Router,
+};
+
+mod controller;
+mod model;
+mod user_service;
+
+#[tokio::main]
+async fn main() {
+    println!("Starting Service..!");
+
+    let service = UserService::new().await.unwrap();
+
+    let app = Router::new()
+        .route("/users", get(list_users))
+        .route("/user/:id", get(get_user_by_id))
+        .route("/user", post(create_user))
+        .route("/user/:id", put(update_user))
+        .route("/user/:id", delete(delete_user))
+        .layer(Extension(service));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening..!");
+    axum::serve(listener, app).await.unwrap();
 }
---
+++ src/user_service.rs
@@ -0,0 +1,73 @@
+use std::env;
+
+use crate::model::{User, UserInfo};
+use dotenvy;
+use sqlx::{postgres::PgPoolOptions, Error, PgPool};
+
+#[derive(Clone)]
+pub struct UserService {
+    pool: PgPool,
+}
+
+impl UserService {
+    pub async fn new() -> Result<Self, Error> {
+        let url = dotenvy::var("DATABASE_URL").expect("Expected Database URL..!");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&url)
+            .await?;
+        Ok(Self { pool })
+    }
+
+    pub async fn list_users(&self) -> Result<Vec<User>, Error> {
+        let users =
+            sqlx::query_as::<_, User>("SELECT id, name, occupation, email, phone FROM users")
+                .fetch_all(&self.pool)
+                .await?;
+        Ok(users)
+    }
+
+    pub async fn get_user_by_id(&self, id: i32) -> Result<User, Error> {
+        let user = sqlx::query_as::<_, User>(
+            "SELECT id, name, occupation, email, phone FROM users WHERE id = $1",
+        )
+        .bind(id)
+        .fetch_one(&self.pool)
+        .await?;
+        Ok(user)
+    }
+
+    pub async fn create_user(&self, user: UserInfo) -> Result<(), Error> {
+        sqlx::query("INSERT INTO users (name, ocupation, email, phone) VALUES ($1, $2, $3, $4)")
+            .bind(user.name)
+            .bind(user.occupation)
+            .bind(user.email)
+            .bind(user.phone)
+            .execute(&self.pool)
+            .await?;
+        Ok(())
+    }
+
+    pub async fn update_user(&self, id: i32, user: UserInfo) -> Result<(), Error> {
+        sqlx::query(
+            "UPDATE users SET name =$1, ocupation = $2, email = $3, phone =$4 WHERE id = $5",
+        )
+        .bind(user.name)
+        .bind(user.occupation)
+        .bind(user.email)
+        .bind(user.phone)
+        .bind(id)
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+
+    pub async fn delete_user(&self, id: i32) -> Result<(), Error> {
+        sqlx::query("DELETE FROM users WHERE id = $1")
+            .bind(id)
+            .execute(&self.pool)
+            .await?;
+        Ok(())
+    }
+}
